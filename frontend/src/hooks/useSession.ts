182f71109b20979ac01ec35ff5f08169f1905e76
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-06-02 14:18:08 +0200

    changed the apibase url to static

--- frontend/src/hooks/useSession.ts
+++ frontend/src/hooks/useSession.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000";
+const apiBaseUrl = "https://assignmentdeploywebapp-backend-qvcj.onrender.com/";
 
 type SessionState = "loading" | "authenticated" | "unauthenticated";
 
